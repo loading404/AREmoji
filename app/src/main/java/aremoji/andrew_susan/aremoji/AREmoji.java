6a3ad781eb34f295b32b76eb0879b1d6cc61df5c
ARG <argat1995@gmail.com>
2018-05-25 14:27:38 -0700

    Initial commit

---
+++ app/src/main/java/aremoji/andrew_susan/aremoji/AREmoji.java
@@ -0,0 +1,13 @@
+package aremoji.andrew_susan.aremoji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AREmoji extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aremoji);
+    }
+}
